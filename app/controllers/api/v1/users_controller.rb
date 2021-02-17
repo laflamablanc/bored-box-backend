20d12f8e10c930f3d33fcff57a0dee7810884828
Sean LaFlam <sean.laflam@gmail.com>
2021-02-17 12:40:58 -0500

    Token Working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,30 +3,30 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
     def index
-        users = User.all
-        render json: users
-      end
+      users = User.all
+      render json: users
+    end
     
       def create
-        byebug
+        # byebug
         user = User.create(user_params)
         if user.valid?
           render json: { user: UserSerializer.new(user)}, status: :created
         else
           render json: { error: 'failed to create user'}, status: :not_acceptable
         end
-      end
+    end
     
-      def show
-        user = User.find(params[:id])
-        render json: user
-      end
+    def show
+      user = User.find(params[:id])
+      render json: user
+    end
     
       private
     
-      def user_params
-        params.require(:user).permit(:username, :email, :password, :name, :zip, :address, :subscribed,
-          :board_score, :card_score, :party_score, :fantasy_score, :economic_score, :territory_score, :players_score,
-          boxes_attributes: [:id, games_attributes: %i[id name price]])
-      end
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :name, :zip, :address, :subscribed,
+        :board_score, :card_score, :party_score, :fantasy_score, :economic_score, :territory_score, :players_score,
+        boxes_attributes: [:id, games_attributes: %i[id name price]])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,38 @@
 Rails.application.routes.draw do
+
+  
+
   namespace :api do
     namespace :v1 do
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+
       resources :users
       resources :user_games
       resources :user_likes
       resources :games
       resources :box_games
       resources :boxes
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
+
     end
   end
 end
 
 
-Rails.application.routes.draw do
-  resources :user_games
-  resources :user_likes
-  resources :games
-  resources :box_games
-  resources :boxes
+# Rails.application.routes.draw do
+#   resources :user_games
+#   resources :user_likes
+#   resources :games
+#   resources :box_games
+#   resources :boxes
 
-  get "/login", to: 'users#login', as: "login"
-  post "/login", to: 'users#handle_login'
+#   get "/login", to: 'users#login', as: "login"
+#   post "/login", to: 'users#handle_login'
 
-  namespace :api do
-    namespace :v1 do
+#   namespace :api do
+#     namespace :v1 do
 
-      resources :users
-    end
-  end
-end
+#       resources :users
+#     end
+#   end
+# end
