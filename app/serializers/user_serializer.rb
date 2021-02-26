27a89b5530bb0c35630d55b27b4fe6583bfc49c9
Sean LaFlam <sean.laflam@gmail.com>
2021-02-25 23:40:14 -0500

    User Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,12 +2,8 @@ class UserSerializer < ActiveModel::Serializer
 
   attributes :id, :username, :address, :boxes, :subscribed, :collection, :user_games, :user_profile, :game_recs
 
-  # :board_score, :fantasy_score, :players_score,
-
   def boxes
     ActiveModel::SerializableResource.new(object.boxes, each_serializer: BoxSerializer)
   end
-
-
 
 end
